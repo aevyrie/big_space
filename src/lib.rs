0907ec1d94290ec292360db7e4775632b206c2cf
atomicbeef <atomicbeef@users.noreply.github.com>
2023-11-10 09:55:23 +0200

    Use a SystemSet for system ordering (#5)
    I ran into an issue when trying to add the transform propagation systems
    to a schedule more than once. The app would panic because there was more
    than one instance of `propagate_transforms::<P>` and
    `recenter_transform_on_grid::<P>` in the system graph, so Bevy didn't
    know how to actually resolve the dependencies. By using a `SystemSet`,
    we can avoid this issue nicely.
    
    Unfortunately, I couldn't come up with a good name for the set, so I
    decided to stick the definition inside the `build()` method itself. That
    way at least the poor naming won't be exposed 😛. It still feels a little
    icky to me though.
    
    ---------
    
    Co-authored-by: Aevyrie <aevyrie@gmail.com>
    Co-authored-by: Pyxrs <simplycmd00@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -133,6 +133,9 @@ impl<P: GridPrecision> FloatingOriginPlugin<P> {
 
 impl<P: GridPrecision + Reflect + FromReflect + TypePath> Plugin for FloatingOriginPlugin<P> {
     fn build(&self, app: &mut App) {
+        #[derive(Debug, Hash, PartialEq, Eq, Clone, SystemSet)]
+        struct RootGlobalTransformUpdates;
+
         app.insert_resource(FloatingOriginSettings::new(
             self.grid_edge_length,
             self.switching_threshold,
@@ -144,28 +147,20 @@ impl<P: GridPrecision + Reflect + FromReflect + TypePath> Plugin for FloatingOri
         .add_systems(
             PostStartup,
             (
-                recenter_transform_on_grid::<P>,
-                sync_simple_transforms::<P>
-                    .after(recenter_transform_on_grid::<P>)
-                    .before(propagate_transforms::<P>),
-                update_global_from_grid::<P>
-                    .after(recenter_transform_on_grid::<P>)
-                    .before(propagate_transforms::<P>),
-                propagate_transforms::<P>,
+                recenter_transform_on_grid::<P>.before(RootGlobalTransformUpdates),
+                (sync_simple_transforms::<P>, update_global_from_grid::<P>)
+                    .in_set(RootGlobalTransformUpdates),
+                propagate_transforms::<P>.after(RootGlobalTransformUpdates),
             )
                 .in_set(TransformSystem::TransformPropagate),
         )
         .add_systems(
             PostUpdate,
             (
-                recenter_transform_on_grid::<P>,
-                sync_simple_transforms::<P>
-                    .after(recenter_transform_on_grid::<P>)
-                    .before(propagate_transforms::<P>),
-                update_global_from_grid::<P>
-                    .after(recenter_transform_on_grid::<P>)
-                    .before(propagate_transforms::<P>),
-                propagate_transforms::<P>,
+                recenter_transform_on_grid::<P>.before(RootGlobalTransformUpdates),
+                (sync_simple_transforms::<P>, update_global_from_grid::<P>)
+                    .in_set(RootGlobalTransformUpdates),
+                propagate_transforms::<P>.after(RootGlobalTransformUpdates),
             )
                 .in_set(TransformSystem::TransformPropagate),
         );
@@ -272,9 +267,14 @@ impl FloatingOriginSettings {
 #[derive(Bundle, Default)]
 pub struct FloatingSpatialBundle<P: GridPrecision> {
     /// The visibility of the entity.
+    #[cfg(feature = "bevy_render")]
     pub visibility: Visibility,
-    /// The computed visibility of the entity.
-    pub computed: ComputedVisibility,
+    /// The inherited visibility of the entity.
+    #[cfg(feature = "bevy_render")]
+    pub inherited: InheritedVisibility,
+    /// The view visibility of the entity.
+    #[cfg(feature = "bevy_render")]
+    pub view: ViewVisibility,
     /// The transform of the entity.
     pub transform: Transform,
     /// The global transform of the entity.
@@ -296,7 +296,7 @@ pub fn recenter_transform_on_grid<P: GridPrecision>(
 ) {
     query
         .par_iter_mut()
-        .for_each_mut(|(mut grid_pos, mut transform)| {
+        .for_each(|(mut grid_pos, mut transform)| {
             if transform.as_ref().translation.abs().max_element()
                 > settings.maximum_distance_from_origin
             {
@@ -326,14 +326,14 @@ pub fn update_global_from_grid<P: GridPrecision>(
         let mut all_entities = entities.p1();
         all_entities
             .par_iter_mut()
-            .for_each_mut(|(local, global, entity_cell)| {
+            .for_each(|(local, global, entity_cell)| {
                 update_global_from_cell_local(&settings, entity_cell, &origin_cell, local, global);
             });
     } else {
         let mut moved_cell_entities = entities.p0();
         moved_cell_entities
             .par_iter_mut()
-            .for_each_mut(|(local, global, entity_cell)| {
+            .for_each(|(local, global, entity_cell)| {
                 update_global_from_cell_local(&settings, entity_cell, &origin_cell, local, global);
             });
     }
@@ -368,7 +368,7 @@ pub fn sync_simple_transforms<P: GridPrecision>(
 ) {
     query
         .par_iter_mut()
-        .for_each_mut(|(transform, mut global_transform)| {
+        .for_each(|(transform, mut global_transform)| {
             *global_transform = GlobalTransform::from(*transform);
         });
 }
