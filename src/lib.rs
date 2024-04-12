0dafa2b83c423664df8dcf91c85d2d1d41a7bda5
Martin Svanberg <martin@martinsvanberg.com>
2024-04-12 06:09:04 +0200

    Use get_single instead of single (#15)
    `big_space` currently panics if you defer creation of the floating
    origin. Using `get_single` instead of `single` prevents this.

--- src/lib.rs
+++ src/lib.rs
@@ -322,7 +322,9 @@ pub fn update_global_from_grid<P: GridPrecision>(
         Query<(GridTransformReadOnly<P>, &mut GlobalTransform)>,
     )>,
 ) {
-    let (origin_cell, floating_origin) = origin.single();
+    let Ok((origin_cell, floating_origin)) = origin.get_single() else {
+        return;
+    };
 
     if origin_cell.is_changed() || floating_origin.is_changed() {
         let mut all_entities = entities.p1();
